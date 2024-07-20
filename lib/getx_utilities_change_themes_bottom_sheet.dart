f22024402087f32deca438e2e4fde956b7b47758
Muhammad Ali <contactsyedmuhammadali@gmail.com>
2024-07-20 19:27:50 +0500

    Initial commit which includes basic getx utilities

---
+++ lib/getx_utilities_change_themes_bottom_sheet.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+//This class includes some of the getx utilities such as bottom bar and how
+// we can easily change the theme with getx, and also Snack Bar with getx
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Getx Tutorials')),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+                elevation: 8,
+                color: Colors.blueGrey,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0)),
+                child: ListTile(
+                  title: const Text('Getx Default Dialog'),
+                  subtitle: const Text(
+                    'Learning Gex Utility Default Dialog',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  leading: const Icon(
+                    Icons.person,
+                    color: Colors.black,
+                  ),
+                  onTap: () {
+                    Get.defaultDialog(
+                        title: 'Muhammad Ali Bukhari',
+                        backgroundColor: Colors.deepPurpleAccent,
+                        middleText:
+                            'I am Muhammad Ali Bukhari.I  want to learn flutter and web development and want to become a full stack developer',
+                        confirm: TextButton(
+                            onPressed: () {
+                              Get.back();
+                            },
+                            child: const Text(
+                              'OK',
+                              style: TextStyle(color: Colors.black),
+                            )),
+                        cancel: TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Cancel',
+                              style: TextStyle(color: Colors.black),
+                            )));
+                  },
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+                elevation: 8,
+                color: Colors.blueGrey,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0)),
+                child: ListTile(
+                  title: const Text('Getx Bottom Bar Sheet'),
+                  subtitle: const Text(
+                    'Learning Gex Utility Bottom Bar Sheet ',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  leading: const Icon(
+                    Icons.person,
+                    color: Colors.black,
+                  ),
+                  onTap: () {
+                    Get.bottomSheet(
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30)),
+                        child: Column(
+                          children: [
+                            ListTile(
+                              leading: const Icon(Icons.light_mode),
+                              title: const Text('Light Mode'),
+                              onTap: () {
+                                Get.changeTheme(ThemeData.light());
+                              },
+                            ),
+                            ListTile(
+                              leading: const Icon(Icons.dark_mode),
+                              title: const Text('Dark Mode'),
+                              onTap: () {
+                                Get.changeTheme(ThemeData.dark());
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      backgroundColor: Colors.redAccent,
+                    );
+                  },
+                )),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Get.snackbar('Muhammad Ali Bukhari',
+              'Learning Flutter with passion and dedication',
+              backgroundColor: Colors.redAccent,
+              snackPosition: SnackPosition.TOP);
+        },
+        backgroundColor: Colors.blueAccent,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_tutorials_material/getx_utilities_change_themes_bottom_sheet.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      darkTheme: ThemeData(primaryColor: Colors.black45),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
